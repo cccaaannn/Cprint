820086810086e9339f267e6e9340c3c948ffdc5a
Can Kurt <can.kurt.aa@gmail.com>
2019-08-13 01:44:13 +0300

    first commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
      name='extprint',  
-     version='0.2',
+     version='0.3',
      scripts=['extprint'] ,
      author="Can Kurt",
      author_email="can.kurt.aa@gmail.com",
