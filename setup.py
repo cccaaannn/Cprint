1b941b3ee293fcb7211f0faf6f945f4c344506df
Can Kurt <can.kurt.aa@gmail.com>
2019-08-13 01:43:14 +0300

    first commit

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-import setuptools
+from distutils.core import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(
+setup(
      name='extprint',  
-     version='0.1',
+     version='0.2',
      scripts=['extprint'] ,
      author="Can Kurt",
      author_email="can.kurt.aa@gmail.com",
      description="prints stuff",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/cccaaannn/Cprint",
-     packages=setuptools.find_packages(),
+     url="https://github.com/cccaaannn/extprint",
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
