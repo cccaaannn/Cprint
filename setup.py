6512024fc35b557d7b47e5a0c80a2c3ab0658c1d
Can Kurt <can.kurt.aa@gmail.com>
2019-08-13 01:51:31 +0300

    first commit

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='extprint',  
-    version='0.5',
-    packages=['extprint'] ,
-    author="Can Kurt",
-    author_email="can.kurt.aa@gmail.com",
-    description="prints stuff",
-    url="https://github.com/cccaaannn/extprint"
+    name='extprint',
+    version='0.2',
+    packages=['extprint',],
+    license='mit'
 )
